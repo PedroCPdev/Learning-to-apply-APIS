acc92c81b550dd0a386f9ec6cf707b05692c5ba9
PedroCPdev <pedrochasci@gmail.com>
2025-06-02 17:07:42 -0300

    Implementacao inicial do curso de API com SPRINGBOOT

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,31 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		Scanner scan = new Scanner(System.in);
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t="+ scan.nextLine().replace(" ", "+") +"&apikey=becabc03");
+		System.out.println(json);
+		ConverteDados converteDados = new ConverteDados();
+		DadosSerie dados = converteDados.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
